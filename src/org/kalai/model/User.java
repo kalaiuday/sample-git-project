9e1947f54f1b2783fea22337bcea0cac43bad956
Kalai Uday <kalaiuday22@gmail.com>
2022-05-02 11:01:07 -0500

    Added getter and setter for the name field.

--- src/org/kalai/model/User.java
+++ src/org/kalai/model/User.java
@@ -3,6 +3,15 @@ package org.kalai.model;
 public class User {
 
 	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 	
 
 }
