e019288588214acdf8e09771834b288168b7c717
Kalai Uday <kalaiuday22@gmail.com>
2022-05-02 13:40:29 -0500

    Added age field and getter setter

--- src/org/kalai/model/User.java
+++ src/org/kalai/model/User.java
@@ -3,6 +3,7 @@ package org.kalai.model;
 public class User {
 
 	private String name;
+	private int age;
 
 	public String getName() {
 		return name;
@@ -11,6 +12,16 @@ public class User {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
 	
 	
 
